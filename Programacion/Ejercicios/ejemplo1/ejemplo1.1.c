0d11b9f5fbd5290e457547a6171d04ff315919dd
lucas061971 <lucasmediaroble@gmail.com>
2024-10-22 14:22:05 +0200

    Ordenando

---
+++ Programacion/Ejercicios/ejemplo1/ejemplo1.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+
+	char str1[50];
+	printf("Introduce una palabra: \n");
+	scanf("%s", str1);
+
+	int caracter;
+	printf("Qué caracter quieres poner:");
+	scanf("%d", %caracter);
+
+	int sobreponer;
+	printf("Qué caracter quieres poner en su lugar?: ");
+	str1[caracter + 1] = sobreponer;
+
+	printf("%s\n", str1);
+	
+
+	return 0;
+
+}
